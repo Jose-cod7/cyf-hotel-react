535ffa2626d61dde48f26b8b4b04c9ec74250200
Jose-cod7 <josegarriaga7@gmail.com>
2020-06-15 18:22:05 +0200

    cyf-hotel-3

--- src/App.js
+++ src/App.js
@@ -6,10 +6,30 @@ import TouristInfoCards from "./components/TouristInfoCards";
 import "./App.css";
 
 const App = () => {
+  const touristWebsites = [
+    {
+      name: "Glasgow",
+      url: "https://peoplemakeglasgow.com",
+      img:
+        "https://viajarporescocia.com/contenido/uploads/foto_principal-19.jpg"
+    },
+    {
+      name: "Manchester",
+      url: "https://visitmanchester.com",
+      img:
+        "https://www.cerodosbe.com/uploads/s1/99/83/63/0/panoramica-3-turismo-de-manchester_16_643x397.jpeg"
+    },
+    {
+      name: "London",
+      url: "https://visitlondon.com",
+      img:
+        "https://images.musement.com/cover/0002/49/big-ben-westminster-bridge-on-river-thames-in-london-jpg_header-148518.jpeg?w=1200&h=630&q=95&fit=crop"
+    }
+  ];
   return (
     <div className="App">
       <Heading />
-      <TouristInfoCards />
+      <TouristInfoCards cards={touristWebsites} />
       <Bookings />
     </div>
   );
--- src/components/TouristInfoCards.js
+++ src/components/TouristInfoCards.js
@@ -1,31 +1,11 @@
 import React from "react";
 
-const TouristInfoCards = () => {
-  const touristWebsites = [
-    {
-      name: "Glasgow",
-      url: "https://peoplemakeglasgow.com",
-      img:
-        "https://viajarporescocia.com/contenido/uploads/foto_principal-19.jpg"
-    },
-    {
-      name: "Manchester",
-      url: "https://visitmanchester.com",
-      img:
-        "https://www.cerodosbe.com/uploads/s1/99/83/63/0/panoramica-3-turismo-de-manchester_16_643x397.jpeg"
-    },
-    {
-      name: "London",
-      url: "https://visitlondon.com",
-      img:
-        "https://images.musement.com/cover/0002/49/big-ben-westminster-bridge-on-river-thames-in-london-jpg_header-148518.jpeg?w=1200&h=630&q=95&fit=crop"
-    }
-  ];
+const TouristInfoCards = props => {
   return (
     <div>
-      {touristWebsites.map(city => {
+      {props.cards.map(city => {
         return (
-          <div className="cards">
+          <div key={city.name} className="cards">
             <div className="card">
               <img
                 src={city.img}
